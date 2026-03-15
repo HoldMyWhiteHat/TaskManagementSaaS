fc52a1553ad9eddceef01129f028c338e282927b
KeepMyWhiteHat <cs04342@uoi.gr>
2026-03-15 20:11:41 +0200

    Add project files.

---
+++ TaskManagementSaaS.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementSaaS.Domain.Entities;
+
+namespace TaskManagementSaaS.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tenant> Tenants { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
+// REPRESENTS MY DATABASE CONTEXT FOR MY APPLICATION
